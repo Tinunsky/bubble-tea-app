de180024669b94275b5b50e0a44b5c740b12065f
Khrystsina  Kozak <36383143+Tinunsky@users.noreply.github.com>
2024-06-12 17:53:17 +0200

    favicon, background app, vercel config

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import { CSSProperties } from "react";
 
 const containerStyle: CSSProperties= {
   margin: "auto",
-  boxShadow: "rgb(46 7 8) 0px 0px 60px, rgb(46 7 8) 0px 0px 19px",
+  background: "white md:hidden",
+  boxShadow: "hidden md:rgb(46 7 8) 0px 0px 60px, hidden md:rgb(46 7 8) 0px 0px 19px",
   overflowX: "hidden",
 };
 
